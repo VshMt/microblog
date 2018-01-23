cc188761adf5b4c7028fadfd212b4e6515142674
Vla La <syktyvkarec@mail.ru>
2018-01-23 13:21:52 +0300

    initial project version

---
+++ app/forms.py
@@ -0,0 +1,45 @@
+﻿from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import TextAreaField
+from wtforms.validators import Length
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Эл.поочта', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повтор пароля', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрировать')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста введите другое имя пользователя.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста введите другой адрес.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    about_me = TextAreaField('О себе', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Сохранить изменения')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Пожалуйста измените имя пользователя.')
